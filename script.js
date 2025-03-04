fe6a4c106bbad83e6fc55a4846eb266add5a2abd
tehotur <erteplicek@gmail.com>
2025-03-04 20:06:08 +0100

    first draft, added content sections

---
+++ script.js
@@ -0,0 +1,38 @@
+
+const menuToggle = document.querySelector(".menu-toggle");
+const navbar = document.querySelector(".navbar ul");
+
+
+menuToggle.addEventListener("click", (event) => {
+    navbar.classList.toggle("active");
+    event.stopPropagation();
+});
+
+
+document.addEventListener("click", (event) => {
+    if (!navbar.contains(event.target) && !menuToggle.contains(event.target)) {
+        navbar.classList.remove("active");
+    }
+});
+
+
+const images = [];
+for (let i = 1; i <= 17; i++) {
+    images.push(`slider-pictures/img${i}.jpeg`);
+}
+
+let currentIndex = 0;
+
+function showImage(index) {
+    document.getElementById("sliderImage").src = images[index];
+}
+
+function nextImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    showImage(currentIndex);
+}
+
+function prevImage() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    showImage(currentIndex);
+}
